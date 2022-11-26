8794ba240c8a227d8b41592d88ceeb6f3d429bbc
khushboo-jft <118184391+khushboo-jft@users.noreply.github.com>
2022-11-27 00:21:29 +0530

    Add files via upload

---
+++ crud2/api.js
@@ -0,0 +1,74 @@
+function employee(){
+    this.empData=[];
+    //add details 
+   
+    this.post=(obj)=>{
+        const p1=new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                this.empData=JSON.parse(localStorage.getItem("data"))||[];
+                this.empData.push(obj);
+                resolve(this.empData);
+                localStorage.setItem("data",JSON.stringify(this.empData));
+            }, 2000);
+           
+        });
+        return p1;
+        
+    }
+
+    this.delete=(obj)=>{
+        const p2=new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                this.empData=JSON.parse(localStorage.getItem("data"))||[];
+                this.empData.splice(this.empData.findIndex((a)=>a.id===obj),1);
+                resolve(this.empData);
+                localStorage.setItem("data",JSON.stringify(this.empData));
+            }, 2000);
+        })
+        return p2;
+    }
+
+    this.get=(id)=>{
+        const p3=new Promise((resolve, reject) => {
+            setTimeout(() => {
+
+                this.empData=JSON.parse(localStorage.getItem("data"))||[];
+                for(let i=0;i<this.empData.length;i++){
+                    if(this.empData[i].id==id){
+                        resolve(this.empData[i]);
+                        break;
+                    }
+                }
+            
+            }, 2000);
+
+            
+        })
+        return p3;
+    }
+
+    this.put=(obj)=>{
+        const p4=new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                this.empData=JSON.parse(localStorage.getItem("data"))||[];
+                for(let i=0;i<this.empData.length;i++){
+                    if(this.empData[i].id==obj.id){
+                        this.empData[i]=obj;
+                        break;
+                    }
+                }
+                resolve(this.empData);
+                localStorage.setItem("data",JSON.stringify(this.empData));
+
+            }, 2000);
+        })
+        return p4;
+    }
+
+  
+    
+
+
+
+
+};
